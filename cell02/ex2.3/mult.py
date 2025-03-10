1cd2b981e2b6f6157b5947a1512df1e7809b1836
Pataradanai007 <40686@prot.ac.th>
2025-03-10 13:33:44 +0700

    Add files via upload

---
+++ cell02/ex2.3/mult.py
@@ -0,0 +1,13 @@
+a = int(input("ใส่เลข : "))
+b = int(input("ใส่เลข : "))
+c = a*b
+
+print(f"{a} x {b} = {c}")
+
+if c > 0:
+    print("The result positive")
+elif c < 0:
+    print("The result negative")
+else:
+    print("The result positive and negative")
+    
