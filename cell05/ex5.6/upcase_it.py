56433b4e00e4a81c418084c30ad7ca56ac8872ca
Pataradanai007 <40686@prot.ac.th>
2025-03-11 10:38:21 +0700

    Add files via upload

---
+++ cell05/ex5.6/upcase_it.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+
+# ตรวจสอบว่ามีพารามิเตอร์หรือไม่
+if len(sys.argv) != 2:  # ถ้าจำนวนพารามิเตอร์ไม่เท่ากับ 1
+    print("none")
+else:
+    # แสดงพารามิเตอร์ในรูปแบบตัวพิมพ์ใหญ่
+    print(sys.argv[1].upper())
