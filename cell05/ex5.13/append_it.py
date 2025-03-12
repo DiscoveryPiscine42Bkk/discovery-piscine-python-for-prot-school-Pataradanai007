96d97b1d3b86244173ab3a7ea3b0312666642833
Pataradanai007 <40686@prot.ac.th>
2025-03-12 14:56:03 +0700

    Update append_it.py

--- cell05/ex5.13/append_it.py
+++ cell05/ex5.13/append_it.py
@@ -1,12 +1,14 @@
-#!/usr/bin/env python3
 import sys
 
 # ตรวจสอบว่ามีพารามิเตอร์หรือไม่
 if len(sys.argv) == 1:
     print("none")
 else:
-    # วนลูปผ่านพารามิเตอร์ที่ส่งเข้ามา
-    for param in sys.argv[1:]:
+    # วนลูปผ่านพารามิเตอร์ที่ส่งเข้ามาด้วย while
+    i = 1
+    while i < len(sys.argv):
+        param = sys.argv[i]
         # ถ้าพารามิเตอร์ไม่ลงท้ายด้วย 'ism' ให้เพิ่ม 'ism' ต่อท้ายและแสดง
         if not param.endswith("ism"):
             print(param + "ism")
+        i += 1
