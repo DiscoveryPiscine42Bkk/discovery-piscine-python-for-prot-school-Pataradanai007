632d2d355d6c544bb5426caba5b9632b38e875fd
Pataradanai007 <40686@prot.ac.th>
2025-03-12 14:58:22 +0700

    Update aff_rev_params.py

--- cell05/ex5.8/aff_rev_params.py
+++ cell05/ex5.8/aff_rev_params.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python3
 import sys
 
 # ตรวจสอบว่ามีพารามิเตอร์มากกว่าหรือเท่ากับ 2 ตัว
 if len(sys.argv) < 2:
     print("none")
 else:
-    # ใช้ reversed() หรือการสลับลำดับรายการ แล้วแสดงผล
-    for param in reversed(sys.argv[1:]):
-        print(param)
+    i = len(sys.argv) - 1  # เริ่มจากตัวสุดท้าย
+    while i > 0:  # หยุดเมื่อถึง sys.argv[0] (ชื่อไฟล์)
+        print(sys.argv[i])
+        i -= 1  # ลดค่า i เพื่อเลื่อนย้อนกลับ
