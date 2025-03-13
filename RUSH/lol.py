7e4d6933b3e197f49461e293757cd33af2dda242
Pataradanai007 <40686@prot.ac.th>
2025-03-13 18:45:53 +0700

    Update lol.py

--- RUSH/lol.py
+++ RUSH/lol.py
@@ -1,15 +1,15 @@
-from chess_game import checkmate  # นำเข้าฟังก์ชัน checkmate จาก checkmate.py
+from chess_game import checkmate  # นำเข้าฟังก์ชัน checkmate จาก chess_game.py
 
 def is_square_board(board):
-    rows = board.splitlines()
-    row_length = len(rows[0])
-    return all(len(row) == row_length for row in rows)
+    rows = board.splitlines() #แยกบรรทัดเป็นลิสต์
+    row_length = len(rows[0]) # ความยาวแถวแรก
+    return all(len(row) == row_length for row in rows) #ตรวจสอบว่ากระดานยาวเท่ากันไหม
 
 def main():
     board2 = """\
 R...
 .K..
-.PP.
+..P.
 ....
 """
 
@@ -17,7 +17,7 @@ R...
     if is_square_board(board2):
         checkmate(board2)  # ตรวจสอบกระดาน 2 (เช็คสี่เหลี่ยมจัตุรัสก่อน)
     else:
-        print("Fail: Board is not square.")
+        print("Fail: Board is not square.") # ถ้าไม่ใช่สี่เหลี่ยมจัตุรัสขึ้น Fail
 
 if __name__ == "__main__":
     main()
